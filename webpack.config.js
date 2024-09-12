ada27446e591986520c83f16427337e6c1d606e1
KwekAudio <erickkweka05@gmail.com>
2024-09-12 20:58:58 +0300

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -4,13 +4,13 @@ module.exports = {
   entry: './src/index.js',  // Ensure this path points to your actual entry file
   output: {
     filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist') // Corrected _dirname to __dirname
+    path: path.resolve(__dirname, 'dist') // Corrected __dirname to __dirname
   },
   mode: 'production',
   resolve: {
     extensions: ['.js', '.json'],
     alias: {
-      '@': path.resolve(__dirname, 'src') // Corrected _dirname to __dirname
+      '@': path.resolve(__dirname, 'src') // Corrected __dirname to __dirname
     }
   }
 };
